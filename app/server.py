4ca746a656ec142fe3d7b3fa58671beb6a7c1e6c
nigel-croft-bluefruit <49199771+nigel-croft-bluefruit@users.noreply.github.com>
2020-09-04 17:52:07 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1Cj_y85dXVjld4KDxhjHZBEUtHKvBbuMh/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Cj_y85dXVjld4KDxhjHZBEUtHKvBbuMh'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddy']
